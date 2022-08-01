dd07ab135c90c04fec5e0c56ca75300045e18e04
Hamza Eşsiz <webcatdev@gmail.com>
2022-08-01 23:20:06 +0300

    basic start

---
+++ data/index.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        image: 'me.jpg',
+        sound: 'watashi.m4a',
+        word: 'watashi',
+        meaning: 'ben'
+    },
+    {
+        image: 'you.jpg',
+        sound: 'anata.m4a',
+        word: 'anata',
+        meaning: 'sen'
+    },
+    {
+        image: 'he.jpg',
+        sound: 'kare.m4a',
+        word: 'kare',
+        meaning: 'o (erkekler için)'
+    },
+    {
+        image: 'she.jpg',
+        sound: 'konojo.m4a',
+        word: 'konojo',
+        meaning: 'o (kadınlar için)'
+    },
+]
